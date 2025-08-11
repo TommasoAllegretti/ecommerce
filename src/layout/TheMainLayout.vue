853cdeda4db49305ede788f81f6cec3f950dc346
Tommaso Allegretti <allegretti.tommaso@gmail.com>
2025-08-11 08:09:17 +0200

    (feat): add basic routing and main layout

---
+++ src/layout/TheMainLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <main class="flex justify-center min-h-screen">
+    <section class="w-full max-w-5xl px-4 sm:px-6 lg:px-8 py-6">
+      <slot></slot>
+    </section>
+  </main>
+</template>
