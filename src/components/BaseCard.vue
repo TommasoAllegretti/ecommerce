6b2c9188cf8a794f4513f9c02a7cc8c9a8b37d67
Tommaso Allegretti <allegretti.tommaso@gmail.com>
2025-08-14 19:28:17 +0200

    (feat): add 404 not found page

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="border border-gray-300 dark:border-gray-700 shadow-md shadow-black/20 dark:shadow-white/20 rounded-2xl p-6 flex flex-col items-center justify-center"
+  >
+    <slot></slot>
+  </div>
+</template>
--- src/layout/TheMainLayout.vue
+++ src/layout/TheMainLayout.vue
@@ -1,10 +1,8 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main class="flex justify-center min-h-screen">
-    <section class="w-full max-w-5xl px-4 sm:px-6 lg:px-8 py-6">
+    <section class="w-full max-w-5xl px-4 sm:px-6 lg:px-8 py-12">
       <slot></slot>
     </section>
   </main>
